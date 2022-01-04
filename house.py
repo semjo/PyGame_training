9f3d1e316e05d7d44d37fb9b3c78e787ab4028d9
semjo <semjo@mail.ru>
2022-01-04 21:52:04 +0500

    Сделал декомпозицию задачи, выделив функцию draw_picture.

--- house.py
+++ house.py
@@ -1,11 +1,24 @@
 import pygame
 
+def draw_picture(screen, x, y, width, height):
+    """
+    Рисует картинку с домиком на фоне травы и неба.
+    :param screen: дисплей pygame, на котором будет изображение
+    :param x: левая координата прямоугольника с рисунком
+    :param y: верхняя координата прямоугольника с рисунком
+    :param width: ширина прямоугольника
+    :param height: высота прямоугольника
+    """
+    print("Типа рисую картинку", x, y, width, height)
 
 pygame.init()
 width, height = screen_size = (300, 200)
 screen = pygame.display.set_mode(screen_size)
 
 # Здесь нужно рисовать
+draw_picture(screen, 0, 0, width, height)
+
+
 
 pygame.display.update()
 
